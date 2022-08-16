4fe5afa465d2d556aaade3acbded52a22fedb574
Rodriguez jose luis <rodriguezjoseluis04@gmail.com>
2022-08-16 19:54:26 -0300

    segunda entrega

---
+++ main.js
@@ -0,0 +1,42 @@
+
+
+
+
+const dolar = 150;
+const euro = 136;
+const yenes = 1.01;
+
+
+
+
+let divisa =  prompt("Seleccione divisa n 1 - DOLARES  n 2 - EUROS - n 3 YENES");
+let monto = parseFloat(prompt("ingrese monto deseado"));
+
+
+
+
+if (divisa =="1"){
+       resultado= dolar * monto
+        console.log( "su conversion es de", resultado)
+}
+if (divisa =="2"){
+    resultado= euro * monto
+     console.log( "su conversion es de", resultado)
+}
+if (divisa =="3"){
+    resultado= yenes * monto
+     console.log( "su conversion es de", resultado)
+}
+    
+if (divisa >=4){
+   
+    alert ("resultado no valido")
+
+
+}
+
+  
+
+ 
+    
+   
