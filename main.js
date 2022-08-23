9a136a7192d6ceab24f51056439769a24cb318ca
Rodriguez jose luis <rodriguezjoseluis04@gmail.com>
2022-08-23 20:28:39 -0300

    correciones

--- main.js
+++ main.js
@@ -7,25 +7,25 @@ const euro = 136;
 const yenes = 1.01;
 
 
-
+while (isNaN(divisa) || isNaN(monto)) {
 
 let divisa =  prompt("Seleccione divisa n 1 - DOLARES  n 2 - EUROS - n 3 YENES");
 let monto = parseFloat(prompt("ingrese monto deseado"));
+}
 
-
-
+function calcular_conversion(monto){
 
 if (divisa =="1"){
-       resultado= dolar * monto
-        console.log( "su conversion es de", resultado)
+      let resultado= dolar * monto
+        console.log( "su conversion es de $",resultado)
 }
 if (divisa =="2"){
     resultado= euro * monto
-     console.log( "su conversion es de", resultado)
+    console.log( "su conversion es de $",resultado)
 }
 if (divisa =="3"){
     resultado= yenes * monto
-     console.log( "su conversion es de", resultado)
+    console.log( "su conversion es de $",resultado)
 }
     
 if (divisa >=4){
@@ -34,8 +34,5 @@ if (divisa >=4){
 
 }
 
-  
+}
 
- 
-    
-   
